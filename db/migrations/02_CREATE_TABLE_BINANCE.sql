a8ad2ad4e7404841423d950e3d0373c7fd4b9ed8
Robert <bibbycodes@gmail.com>
2020-04-18 16:23:47 +0100

    can save to db

--- db/migrations/02_CREATE_TABLE_BINANCE.sql
+++ db/migrations/02_CREATE_TABLE_BINANCE.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
   last_name VARCHAR(200),
   instagram_handle VARCHAR(200),
   description VARCHAR(1600),
-  email_address VARCHAR(400),
+  email VARCHAR(400),
   hashtags VARCHAR(500),
   filepath VARCHAR(600)
 );
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const PORT = process.env.HTTP_PORT || 5000;
 const app = express()
 const cors = require('cors');
 const path = require('path')
+const bodyParser = require('body-parser')
+const dbConn = require('./models/dbConn')
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('/home', (req, res) => {
@@ -13,6 +16,20 @@ app.get('/home', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`)
+})
+
+app.post('/new_image', (req, res) => {
+  console.log(req.body)
+  let request = req.body
+  let db = new dbConn()
+  let queryString = 
+    `INSERT INTO users 
+    (first_name, last_name, instagram_handle, description, email, hashtags, filepath)
+    VALUES ('${request.first_name}', '${request.last_name}', '${request.instagram_handle}', '${request.description}', '${request.email}', '${request.hashtags}', '${request.file_path}')
+    RETURNING *
+    `
+  let response = db.query(queryString)
+  res.send(response)
 })
 
 module.exports = app
